28eae30fd8c7f6c48068961f106ad0757cf336a1
Neko <neko.is.a.dev@gmail.com>
2024-03-11 21:39:47 -0700

    favicon

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,48 @@
+const config = {
+    assetsPath: '/assets/',
+    port: 80
+}
+
 const ServerClass = require('./server')
-const server = new ServerClass(80).start()
+const server = new ServerClass(config.port).start()
 
 function readPath(path) {
-    return require('fs').readFileSync(require('path').join(__dirname, `./frontend/paths/${path}`)).toString('utf-8')
+    return require('fs').readFileSync(require('path').join(__dirname, path))
 }
 
 const pathMap = [
     {
         pathName: '/',
-        pathFile: readPath('main/index.html')
+        pathFile: readPath('./frontend/paths/main/index.html')
+    }
+]
+
+const assets = [
+    {
+        pathName: 'favicon.ico',
+        pathFile: readPath('./assets/favicon.ico')
     }
 ]
 
 server.on('request', (req, res) => {
     const path = req.url || '/'
+
+    if (path.startsWith(config.assetsPath)) {
+        const requestedFilePath = path.substring(config.assetsPath.length)
+
+        const requestedFile = assets.find(asset => asset.pathName === requestedFilePath)
+
+        if (requestedFile) {
+            res.write(requestedFile.pathFile)
+            res.end()
+        } else {
+            res.statusCode = 404
+            res.write('404 - Not found')
+            res.end()
+        }
+
+        return
+    }
     
     const foundPath = pathMap.find(pathData => pathData.pathName === path)
 
