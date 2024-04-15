61872a7dc18273ad8517b46dbf4a751a5c1ddc0c
Neko <neko.is.a.dev@gmail.com>
2024-04-14 21:55:15 -0700

    guh

--- frontend/scripts/info.js
+++ frontend/scripts/info.js
@@ -1,5 +1,6 @@
 (req, res) => {
     let platform = null
+    const toBlock = '::ffff:'
 
     try {
         platform = JSON.parse(req.headers['sec-ch-ua-platform'])
@@ -9,6 +10,7 @@
 
     res.statusCode = 200
     try {
+        if (req.socket.remoteAddress.startsWith(toBlock)) req.socket.remoteAddress = req.socket.remoteAddress.substring(toBlock.length)
         res.write(JSON.stringify(
             {
                 ipAddress: req.socket.remoteAddress || null,
