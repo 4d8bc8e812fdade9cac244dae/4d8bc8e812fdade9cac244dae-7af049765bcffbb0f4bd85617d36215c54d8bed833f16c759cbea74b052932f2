5fa30c94c0a2801396e9be0910bc08cd3f1ee6eb
Neko <neko.is.a.dev@gmail.com>
2024-04-25 23:34:38 -0700

    add stuff (real)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+process.on('uncaughtException', (e) => console.error(e.stack))
+
 const fs = require('fs')
 
 function readPath(path) {
@@ -23,6 +25,52 @@ try {
         fs.readFileSync('/etc/letsencrypt/live/nekoisa.dev/fullchain.pem')
     )
 } catch {}
+
+const domainMap = [
+    {
+        name: 'nekoisa.dev',
+        custom: false
+    },
+    {
+        name: 'doin-your.mom',
+        custom: {
+            pathMap: [
+                {
+                    pathName: '',
+                    pathFile: readPath('./frontend/systemPaths/domains/doin-your.mom/index.html'),
+                    runAsJavascript: false,
+                },
+                {
+                    pathName: '0.png',
+                    pathFile: readPath('./frontend/systemPaths/domains/doin-your.mom/index.html'),
+                    runAsJavascript: false,
+                },
+            ],
+            assets: [],
+            example: [],
+            notFound: readPath('./frontend/systemPaths/domains/global/nginx_notfound.html'),
+            toggles: {
+                assets: false,
+                example: false,
+                notFound: true,
+            }
+        }
+    },
+    {
+        name: 'ifyouseethisyoulikemenkissing.xyz',
+        custom: {
+            pathMap: [],
+            assets: [],
+            example: [],
+            notFound: readPath('./frontend/systemPaths/domains/global/nginx_notfound.html'),
+            toggles: {
+                assets: false,
+                example: false,
+                notFound: true,
+            }
+        }
+    }
+]
 
 const pathMap = [
     {
@@ -115,76 +163,161 @@ fs.readdirSync(config.exampleFilePath).forEach(dir => {
 })
 
 server.on('request', (req, res) => {
-    if (!req.method) req.method = 'GET'
+    let host = ''
+    let domain = domainMap[0]
     let path = req.url || ''
+    if (req && req.headers && req.headers['host']) host = req.headers['host']
+    if (host) domain = domainMap.find(domain => domain.name === host)
+    else domain = domainMap[0]
+    if (!req.method) req.method = 'GET'
     if (path.endsWith('/')) path = path.slice(0, -1)
 
-    if (path.startsWith(config.assetsPath)) {
-        const requestedFilePath = path.substring(config.assetsPath.length)
-
-        const requestedFile = assets.find(asset => asset.pathName === requestedFilePath)
-
-        if (requestedFile) {
-            res.statusCode = 200
-            res.write(requestedFile.pathFile)
-            res.end()
-        } else {
-            res.statusCode = 404
-            res.write(config.notFound)
-            res.end()
-        }
-
-        return
-    }
-
-    if (path.startsWith(config.examplePath)) {
-        const requestedFilePath = path.substring(config.examplePath.length)
-
-        const requestedFile = example.find(example => example.pathName === requestedFilePath)
-
-        if (requestedFile) {
-            res.statusCode = 200
-            res.write(requestedFile.pathFile)
-            res.end()
-        } else {
-            res.statusCode = 404
-            res.write(config.notFound)
-            res.end()
-        }
-
-        return
-    }
+    if (!domain || !domain.custom) {
+        if (path.startsWith(config.assetsPath)) {
+            const requestedFilePath = path.substring(config.assetsPath.length)
     
-    const foundPath = pathMap.find(pathData => pathData.pathName === path)
-
-    if (foundPath) {
-        if (foundPath.allowedTypes) {
-            const allowed = foundPath.allowedTypes.findIndex(type => type === req.method)
-            if (allowed === -1) {
-                res.statusCode = 400
-                res.write(`Invalid request method: ${req.method} expected ${foundPath.allowedTypes.join(' ')}`)
+            const requestedFile = assets.find(asset => asset.pathName === requestedFilePath)
+    
+            if (requestedFile) {
+                res.statusCode = 200
+                res.write(requestedFile.pathFile)
+                res.end()
+            } else {
+                res.statusCode = 404
+                res.write(config.notFound)
+                res.end()
+            }
+    
+            return
+        }
+    
+        if (path.startsWith(config.examplePath)) {
+            const requestedFilePath = path.substring(config.examplePath.length)
+    
+            const requestedFile = example.find(example => example.pathName === requestedFilePath)
+    
+            if (requestedFile) {
+                res.statusCode = 200
+                res.write(requestedFile.pathFile)
+                res.end()
+            } else {
+                res.statusCode = 404
+                res.write(config.notFound)
+                res.end()
+            }
+    
+            return
+        }
+        
+        const foundPath = pathMap.find(pathData => pathData.pathName === path)
+    
+        if (foundPath) {
+            if (foundPath.allowedTypes) {
+                const allowed = foundPath.allowedTypes.findIndex(type => type === req.method)
+                if (allowed === -1) {
+                    res.statusCode = 400
+                    res.write(`Invalid request method: ${req.method} expected ${foundPath.allowedTypes.join(' ')}`)
+                    res.end()
+                    return
+                }
+            }
+            if (foundPath.runAsJavascript) {
+                try {
+                    eval(foundPath.pathFile.toString())(req, res)
+                } catch (e) {
+                    res.statusCode = 500
+                    res.write(`500 Internal server error: ${e.message ? e.message : 'Unknown error'}`)
+                    res.end()
+                }
+                return
+            } else {
+                res.statusCode = 200
+                res.write(foundPath.pathFile)
                 res.end()
                 return
             }
+        } else {
+            res.statusCode = 404
+            res.write(config.notFound)
+            res.end()
         }
-        if (foundPath.runAsJavascript) {
-            try {
-                eval(foundPath.pathFile.toString())(req, res)
-            } catch (e) {
-                res.statusCode = 500
-                res.write(`500 Internal server error: ${e.message ? e.message : 'Unknown error'}`)
+    } else { // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        if (domain.custom.toggles && domain.custom.toggles.assets && domain.custom.assets) {
+            if (path.startsWith(config.assetsPath)) {
+                const requestedFilePath = path.substring(config.assetsPath.length)
+        
+                const requestedFile = domain.assets.find(asset => asset.pathName === requestedFilePath)
+        
+                if (requestedFile) {
+                    res.statusCode = 200
+                    res.write(requestedFile.pathFile)
+                    res.end()
+                } else {
+                    res.statusCode = 404
+                    if (domain.custom.toggles && domain.custom.toggles.notFound && domain.custom.notFound) res.write(domain.custom.notFound)
+                    else res.write(config.notFound)
+                    res.end()
+                }
+        
+                return
+            }
+        }
+
+        if (domain.custom.toggles && domain.custom.toggles.example && domain.custom.example) {
+            if (path.startsWith(config.examplePath)) {
+                const requestedFilePath = path.substring(config.examplePath.length)
+        
+                const requestedFile = domain.assets.find(asset => asset.pathName === requestedFilePath)
+        
+                if (requestedFile) {
+                    res.statusCode = 200
+                    res.write(requestedFile.pathFile)
+                    res.end()
+                } else {
+                    res.statusCode = 404
+                    if (domain.custom.toggles && domain.custom.toggles.notFound && domain.custom.notFound) res.write(domain.custom.notFound)
+                    else res.write(config.notFound)
+                    res.end()
+                }
+        
+                return
+            }
+        }
+        
+        if (domain.custom.toggles && domain.custom.pathMap) {
+            const foundPath = domain.custom.pathMap.find(pathData => pathData.pathName === path)
+
+            if (foundPath) {
+                if (foundPath.allowedTypes) {
+                    const allowed = foundPath.allowedTypes.findIndex(type => type === req.method)
+                    if (allowed === -1) {
+                        res.statusCode = 400
+                        res.write(`Invalid request method: ${req.method} expected ${foundPath.allowedTypes.join(' ')}`)
+                        res.end()
+                        return
+                    }
+                }
+                if (foundPath.runAsJavascript) {
+                    try {
+                        eval(foundPath.pathFile.toString())(req, res)
+                    } catch (e) {
+                        res.statusCode = 500
+                        res.write(`500 Internal server error: ${e.message ? e.message : 'Unknown error'}`)
+                        res.end()
+                    }
+                    return
+                } else {
+                    res.statusCode = 200
+                    res.write(foundPath.pathFile)
+                    res.end()
+                    return
+                }
+            } else {
+                res.statusCode = 404
+                if (domain.custom.toggles && domain.custom.toggles.notFound && domain.custom.notFound) res.write(domain.custom.notFound)
+                else res.write(config.notFound)
                 res.end()
             }
-            return
-        } else {
-            res.statusCode = 200
-            res.write(foundPath.pathFile)
-            res.end()
-            return
         }
-    } else {
-        res.statusCode = 404
-        res.write(config.notFound)
-        res.end()
     }
 })
