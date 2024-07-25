b60a3f3eb25ac278145a09af99525689b38c8b5b
Neko <neko.is.a.dev@gmail.com>
2024-07-25 16:04:10 -0700

    add messages

--- frontend/systemPaths/domains/2hw.icu/bomb.lua
+++ frontend/systemPaths/domains/2hw.icu/bomb.lua
@@ -1,7 +1,10 @@
 redstone.setOutput('top', false)
 
+print('Bomb checking to be armed')
+
 while true do
     if ship.getVelocity().y < -20 then
+        print('Bomb armed')
         while true do
             if ship.getVelocity().y > -10 then
                 redstone.setOutput('top', true)
