ded3670a420cbec89ccded83dd926d81c3ef0f15
Neko <neko.is.a.dev@gmail.com>
2024-07-30 08:54:49 -0700

    update

--- frontend/systemPaths/domains/2hw.icu/signal_1t_rotate.lua
+++ frontend/systemPaths/domains/2hw.icu/signal_1t_rotate.lua
@@ -2,7 +2,8 @@ local sides = {'front', 'right', 'back', 'left'}
 
 while true do
     for i, side in pairs(sides) do
-        redstone.setOutput(side, true)
+        if redstone.getOutput('bottom') == true then redstone.setOutput(side, true)
+        end
         sleep()
         redstone.setOutput(sides[(i - 2) % #sides + 1], false)
     end
